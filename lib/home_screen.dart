e4d869b9719eb216ba8709cad70afa2daaefe0c5
KareemAbbas <karemabas29@gmail.com>
2024-10-04 17:54:44 +0300

    themeApp&quranScreen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:islami/tabs/hadeth/hadeth_tab.dart';
+import 'package:islami/tabs/quran/quran.dart';
+import 'package:islami/tabs/radio/radio_tab.dart';
+import 'package:islami/tabs/sebhah/sebhah_tab.dart';
+import 'package:islami/tabs/settings/setting_tab.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = '/home';
@@ -8,30 +13,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  List<Widget> tabs = [Quran(), Hadeth(), Sebhah(), Radio_tab(), Setting()];
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          items: const [
-            BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage('assets/images/quran.png')),
-                label: 'quran'),
-            BottomNavigationBarItem(
-                icon: ImageIcon(AssetImage('assets/images/hades.png')),
-                label: 'hades'),
-            BottomNavigationBarItem(
-                icon:
-                    ImageIcon(AssetImage('assets/images/sebha_blue.png')),
-                label: 'sebha'),
-            BottomNavigationBarItem(
-                icon:
-                    ImageIcon(AssetImage('assets/images/radio_blue.png')),
-                label: 'radio'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'settings'),
-          ]),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage('assets/images/default_bg.png'),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: Text('إسلامى'),
+        ),
+        body: tabs[currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: currentIndex,
+            onTap: (index) {
+              currentIndex = index;
+              setState(() {});
+            },
+            type: BottomNavigationBarType.fixed,
+            items: const [
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/quran.png')),
+                  label: 'quran'),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/hades.png')),
+                  label: 'hades'),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/sebha_blue.png')),
+                  label: 'sebha'),
+              BottomNavigationBarItem(
+                  icon: ImageIcon(AssetImage('assets/images/radio_blue.png')),
+                  label: 'radio'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: 'settings'),
+            ]),
+      ),
     );
   }
 }
