9187cc52e9ae28097eeb9a5bc2b9950becf20963
KareemAbbas <karemabas29@gmail.com>
2024-10-04 20:21:17 +0300

    quranContent

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color lightPrimary=Color(0xffB7935F);
-  static const Color darkPrimary=Color(0xff141A2E);
-  static const Color white=Color(0xffF8F8F8);
-  static const Color dark=Color(0xff242424);
-  static const Color gold=Color(0xffFACC1D);
+  static const Color lightPrimary = Color(0xffB7935F);
+  static const Color darkPrimary = Color(0xff141A2E);
+  static const Color white = Color(0xffF8F8F8);
+  static const Color dark = Color(0xff242424);
+  static const Color gold = Color(0xffFACC1D);
 
-  static ThemeData lightTheme=ThemeData(
+  static ThemeData lightTheme = ThemeData(
     appBarTheme: AppBarTheme(
-      backgroundColor: Colors.transparent,
-      centerTitle: true,
-      titleTextStyle: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-        color: Colors.black
-      )
-    ),
-    primaryColor:  lightPrimary,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        titleTextStyle: TextStyle(
+            fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black)),
+    scaffoldBackgroundColor: Colors.transparent,
+    primaryColor: lightPrimary,
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: lightPrimary,
-      selectedItemColor: Colors.black,
-      unselectedItemColor: Colors.white
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: lightPrimary,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.white),
+    textTheme: TextTheme(
+        headlineSmall: TextStyle(
+            fontSize: 25, fontWeight: FontWeight.w400, color: Colors.black),
+        titleLarge:TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+          color: Colors.black,
+        ),
     ),
-
   );
-  static ThemeData dartTheme=ThemeData(
+  static ThemeData dartTheme = ThemeData(
     primaryColor: darkPrimary,
   );
-
 }
